5d153c338eec39644444c8034e9590e09ffe1803
Rodrigo Jara <rodri@Rodrigos-MBP.home>
2021-08-20 12:59:46 +0200

    add write and read log functions (counter)

--- src/main.go
+++ src/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var PORT = "8083"
@@ -16,6 +18,43 @@ func main() {
 	}
 }
 
+// visitHandler returns the number of times a request has been made to the server
 func visitHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "visited %s:%s\n", req.Host, PORT)
+	WriteLog(req.Host)
+	count := VisitCount(req.Host)
+	fmt.Fprintf(res,
+		"You are talking to instance %s.\nThis is request %[2]d to this instance and request %[2]d to the cluster.\n",
+		req.Host, count)
+}
+
+// WriteLogs writes a line to instance log file when visited.
+func WriteLog(instance string) {
+
+	ilog, err := os.OpenFile(instance, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0755)
+	defer ilog.Close()
+	if err != nil {
+		log.Println("ERROR opening instance log file", err)
+	}
+
+	log.SetOutput(ilog)
+	log.Println("instance visited")
+
+}
+
+//  VisitCount returns the number of logs (visits) written to instance log file.
+func VisitCount(instance string) int {
+	var count int
+
+	ilog, err := os.Open(instance)
+	defer ilog.Close()
+	if err != nil {
+		log.Println("ERROR opening instance log file", err)
+	}
+
+	scanner := bufio.NewScanner(ilog)
+	for scanner.Scan() {
+		count++
+	}
+
+	return count
 }
